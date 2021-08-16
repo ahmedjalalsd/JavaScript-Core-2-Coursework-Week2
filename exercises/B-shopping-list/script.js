498411726f1c2b9a3f11faa8ffdea66a91d5af4e
Aj <ahmedjalalsd@gmail.com>
2021-08-16 21:25:36 +0100

    B-shopping-list solution

--- exercises/B-shopping-list/script.js
+++ exercises/B-shopping-list/script.js
@@ -1,5 +1,20 @@
 function shoppingList(arrayOfPeople) {
   // Write your code here...
+  let content = document.getElementById("content");
+  let ul = document.createElement("ul");
+  content.appendChild(ul);
+
+  // for (let i = 0; i < arrayOfPeople.length; i++) {
+  //   let li = document.createElement("li");
+  //   li.textContent = arrayOfPeople[i];
+  //   ul.appendChild(li);
+  // }
+
+  arrayOfPeople.forEach((person) => {
+    let li = document.createElement("li");
+    li.textContent = person;
+    ul.appendChild(li);
+  });
 }
 
 let shopping = ["Milk", "Bread", "Eggs", "A Dinosaur", "Cake", "Sugar", "Tea"];
