13d722cc9e92295f72a3b6dc4b33fa8ebce0b5fe
Aj <ahmedjalalsd@gmail.com>
2021-08-16 21:20:29 +0100

    A-list-of-names solution

--- exercises/A-list-of-names/script.js
+++ exercises/A-list-of-names/script.js
@@ -1,5 +1,14 @@
 function listOfNames(arrayOfPeople) {
   let content = document.querySelector("#content");
+
+  arrayOfPeople.forEach((element) => {
+    let h1 = document.createElement("h1");
+    let h2 = document.createElement("h2");
+    h1.textContent = element.name;
+    h2.textContent = element.job;
+    content.appendChild(h1);
+    content.appendChild(h2);
+  });
 }
 
 let people = [
