1b8f480bd781a20403e8623c0da1beb3ba723b9d
Aj <ahmedjalalsd@gmail.com>
2021-08-24 13:38:47 +0100

    E-list-of-colours solution

--- exercises/E-list-of-colours/script.js
+++ exercises/E-list-of-colours/script.js
@@ -1,5 +1,25 @@
 function listOfColours(colours) {
   // Write your code here...
+  let contentEl = document.getElementById("content");
+  let selectEl = document.createElement("select");
+  let parEl = document.createElement("p");
+
+  contentEl.appendChild(selectEl);
+
+  colours.forEach((colour) => {
+    let optionEl = document.createElement("option");
+    optionEl.textContent = colour;
+    selectEl.appendChild(optionEl);
+  });
+
+  document
+    .getElementsByTagName("select")[0]
+    .addEventListener("click", function (evt) {
+      parEl.textContent = "You have selected: " + evt.target.value;
+      parEl.style.color = evt.target.value;
+      contentEl.appendChild(parEl);
+      console.log(evt.target.value);
+    });
 }
 
 const colours = ["red", "blue", "green", "yellow", "pink", "brown"];
